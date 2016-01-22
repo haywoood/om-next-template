6fb1aa9ada789fe193cd0155668b6398ae8c66c1
ryan haywood <haywood@ryans-MacBook-Pro.local>
2016-01-21 21:21:04 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject om-next/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject om-next/lein-template "1.0.0"
+  :description "Lein template to quickly generate what the om next quick start sets you up with"
+  :url "https://github.com/lsdafjklsd/om-next-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
